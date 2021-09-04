fd761c1f985cbf27b7c4a3f483f103fc58d69be8
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2021-09-04 12:50:27 -0400

    init-dev: require auto-tangle-mode

--- init-dev.el
+++ init-dev.el
@@ -6,6 +6,7 @@
 ;; Cleaner to keep this in a separate file rather than a long single line at the top of init.org
 
 ;;; Code:
+(require 'auto-tangle-mode)
 (auto-tangle-mode)
 (add-hook 'auto-tangle-before-tangle-hook #'check-parens)
 (add-hook 'auto-tangle-after-tangle-hook (lambda () (load-file "~/.emacs.d/init.el")))
