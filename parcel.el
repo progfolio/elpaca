ff998814b19d97df7641452cd1d5ceda8b2add11
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2022-03-07 01:05:07 -0500

    parcel--update-order-info: display status buffer when package fails

--- parcel.el
+++ parcel.el
@@ -582,11 +582,14 @@ If INFO is non-nil, ORDER's info is updated as well."
     (when (eq status 'ref-checked-out)
       (mapc #'parcel--continue-mono-repo-dependency (parcel-order-includes order))))
   (when info (parcel--log-event order info))
-  (when (get-buffer-window parcel-status-buffer t) ;status buffer visible
-    (when parcel--order-info-timer (cancel-timer parcel--order-info-timer))
-    (setq parcel--order-info-timer
-          (run-at-time parcel-order-info-debounce-interval
-                       nil #'parcel--print-order-status))))
+  (let ((status-buffer-visible-p (get-buffer-window parcel-status-buffer t)))
+    (if status-buffer-visible-p
+        (progn
+          (when parcel--order-info-timer (cancel-timer parcel--order-info-timer))
+          (setq parcel--order-info-timer
+                (run-at-time parcel-order-info-debounce-interval
+                             nil #'parcel--print-order-status)))
+      (when (eq status 'failed) (parcel-display-status-buffer)))))
 
 (defun parcel--load-cached-autoloads ()
   "Load `parcel--autoloads-cache'."
