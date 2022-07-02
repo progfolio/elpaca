e6761013c5c30f0065c28c0c30398fda3a978587
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2022-07-02 14:56:24 -0400

    elpaca-ui-search-tags: Add #unique tag
    Removes duplicate ITEMs from entries.
    First found is shown.
    This aligns with what source will be used by elpaca-menu-functions.

--- elpaca-ui.el
+++ elpaca-ui.el
@@ -41,6 +41,8 @@ Each element is of the form: (DESCRIPTION PREFIX FACE FUNCTION)."
   '((dirty     . (lambda (entires) (cl-remove-if-not #'elpaca-worktree-dirty-p :key #'car)))
     (declared  . (lambda (entries) (cl-remove-if-not #'elpaca-declared-p :key #'car)))
     (orphan    . (lambda (entries) (cl-remove-if-not #'elpaca-ui--orphan-p entries) :key #'car))
+    (unique    . (lambda (entries) (cl-remove-duplicates entries :key #'car :from-end t)))
+
     (random    . (lambda (entries)
                    (if (< (length entries) 10)
                        entries
