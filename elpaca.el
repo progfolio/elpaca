da8e4e29537dfe3e45f69344487bb8c3e10537e2
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2022-07-05 15:22:13 -0400

    register Elpaca recipe after loading Elpaca
    Prevents recursive load problems when our menus require elpaca.el.
    There may be a better way to handle this, e.g. splitting out the functionality
    menus require into their own sub-feature.

--- elpaca.el
+++ elpaca.el
@@ -1606,11 +1606,5 @@ If FORCE is non-nil,."
                   collect (cons item (plist-put (copy-tree (elpaca<-recipe p)) :ref rev))
                   do (push item seen))))))
 
-;;@TODO: Allow user to reload elpaca with their custom recipe?
-;; Or does this belong in the bootstrap snippet?
-;; This also adds an extra entry to `load-path', because we've added
-;; elpaca's repo directly during bootstrapping.
-(elpaca (elpaca :host github :repo "progfolio/elpaca" :protocol https))
-
 (provide 'elpaca)
 ;;; elpaca.el ends here
