930a1632f4a4d92fc1b233f48bfdd99ae2a10280
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2023-01-09 20:01:58 -0500

    elpaca--continue-build: guard against failed orders

--- elpaca.el
+++ elpaca.el
@@ -636,7 +636,7 @@ If REPLACE is non-nil, the most recent log entry is replaced."
 
 (defun elpaca--continue-build (e)
   "Run E's next build step."
-  (unless (eq (elpaca--status e) 'blocked)
+  (unless (memq (elpaca--status e) '(blocked failed))
     (let ((fn (or (pop (elpaca<-build-steps e)) #'elpaca--finalize)))
       (condition-case-unless-debug err
           (funcall fn e)
