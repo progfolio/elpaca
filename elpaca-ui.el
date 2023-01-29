f7507432d774d6e5ac114fd1c6b4b49105e5e42a
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2023-01-29 18:34:53 -0500

    elpaca-ui-copy: add explicit separator arg for Emacs <= 28

--- elpaca-ui.el
+++ elpaca-ui.el
@@ -570,17 +570,18 @@ The current package is its sole argument."
   "Copy formatted UI view ENTRIES to clipboard."
   (interactive (list (or tabulated-list-entries (user-error "Buffer not in elpaca-ui-mode"))))
   (unless window-system (user-error "Cannot copy in current window-system"))
-  (let ((cols (mapconcat (lambda (col) (format "<th>%s</th>" (car col))) tabulated-list-format))
+  (let ((cols (mapconcat (lambda (col) (format "<th>%s</th>" (car col)))
+                         tabulated-list-format ""))
         (filter elpaca-ui-search-filter))
     (with-temp-buffer
       (insert "<details><summary>Log @" (format-time-string "%Y-%m-%d %H:%M:%S %z")
               "</summary>\n" "search filter: " (format "%S" filter) "\n"
               "<table>\n<thead>" cols "</thead>\n<tbody>\n"
               (mapconcat (lambda (entry)
-                           (format "<tr>%s</tr>\n"
+                           (format "<tr>%s</tr>"
                                    (mapconcat (lambda (data) (concat "<td>" data "</td>"))
-                                              (cl-coerce (cadr entry) 'list))))
-                         entries)
+                                              (cl-coerce (cadr entry) 'list) "")))
+                         entries "\n")
               "</tbody>\n</table>\n</details>")
       (clipboard-kill-region (point-min) (point-max)))
     (message "Elpaca UI view copied to clipboard")))
