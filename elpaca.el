ed138bd2442b26a836e8d1a2606510bbaaffc4f1
Lucius Hu <lebensterben@users.noreply.github.com>
2023-04-19 18:27:36 -0400

    fixup! fixup! fix: correct types for some customization options

--- elpaca.el
+++ elpaca.el
@@ -67,7 +67,7 @@
                                  (failed   . elpaca-failed)
                                  (busy     . elpaca-busy))
   "Alist mapping order statuses to faces."
-  :type '(alist :key-type (:options (blocked finished failed busy))
+  :type '(alist :key-type symbol (:options (blocked finished failed busy))
                 :value-type face))
 
 (defvar elpaca--pre-built-steps
