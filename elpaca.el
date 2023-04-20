90b85fb5213ef35c886c059c932ff61fabbaf819
Lucius Hu <lebensterben@users.noreply.github.com>
2023-04-19 22:46:05 -0400

    fixup! Update elpaca.el

--- elpaca.el
+++ elpaca.el
@@ -866,7 +866,7 @@ If it matches, the E associated with process has its STATUS updated."
          (timer   (process-get process :timer))
          (chunk   (concat parsed output))
          (lines   (split-string chunk "\n"))
-         (returnp (string-match-p "^M" chunk))
+         (returnp (string-match-p "" chunk))
          (linep   (string-empty-p (car (last lines)))))
     (unless (process-get process :messaged)
       (elpaca--signal e (elpaca--command-string (process-command process)) nil nil 1)
@@ -880,7 +880,7 @@ If it matches, the E associated with process has its STATUS updated."
       (setq lines (butlast lines)))
     (dolist (line lines)
       (unless (string-empty-p line)
-        (elpaca--signal e (car (last (split-string line "^M" t)))
+        (elpaca--signal e (car (last (split-string line "" t)))
                         (and pattern (string-match-p pattern line) status) returnp)))
     (when (and pattern (string-match-p pattern output))
       (if (eq status 'failed)
