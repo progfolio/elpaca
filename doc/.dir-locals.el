4e01967342029b007de805404a12ea8a0f17c67b
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2023-04-23 00:20:47 -0400

    doc: add spellcheck localwords

---
+++ doc/.dir-locals.el
@@ -0,0 +1,14 @@
+((nil . ((ispell-buffer-session-localwords
+          . ( "ELPA"
+              "Elisp"
+              "Elpaca"
+              "GC"
+              "MELPA"
+              "TODO"
+              "TODOS"
+              "UI"
+              "alist"
+              "autoload"
+              "emacs"
+              "eval"
+              "init")))))
