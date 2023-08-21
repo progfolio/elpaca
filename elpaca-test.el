7f55b1eec52c0a8c2f8b20d5b6aa37f8eac40f73
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2023-08-21 12:06:25 -0400

    add elpaca-test-log
    Convenience function for printing logs in non-interactive tests

--- elpaca-test.el
+++ elpaca-test.el
@@ -139,6 +139,17 @@ If DELETE is non-nil, delete test environment."
                            (when (fboundp 'markdown-mode) (markdown-mode)))
       (run-with-idle-timer 1 nil (lambda () (pop-to-buffer buffer) (goto-char (point-min)))))))
 
+(declare-function elpaca-log "elpaca-log")
+;;;###autoload
+(defun elpaca-test-log (&rest queries)
+  "Print `elpaca-log' QUERIES."
+  (dolist (q queries)
+    (with-current-buffer (elpaca-log q)
+      (message "elpaca-log: %S\n%S" q
+               (buffer-substring-no-properties
+                (save-excursion (goto-char (point-min)) (line-end-position))
+                (point-max))))))
+
 ;;;###autoload
 (defmacro elpaca-test (&rest body)
   "Test Elpaca in a clean environment.
