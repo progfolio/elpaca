d7b68831b077e115277ac337dec8fb6ecd5746d1
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2023-10-13 10:06:08 -0400

    elpaca-menu-melpa: generalize for other MELPA-like ELPAs
    A feature request was made to support "jcs-elpa"[1] which is configured via
    "github-elpa"[2]. While I don't plan on adding out of the box support for that
    ELPA, its structure is similar enough to MELPA where it
    it can be supported by abstracting MELPA menu configuration into variables.
    This should also make maintaining the MELPA menu easier should anything change.
    
    [1]: https://github.com/jcs-emacs/jcs-elpa/
    [2]: https://github.com/10sr/github-elpa
    
    see: https://github.com/progfolio/elpaca/issues/190

--- elpaca-menu-melpa.el
+++ elpaca-menu-melpa.el
@@ -27,14 +27,20 @@
 (require 'elpaca)
 (require 'url)
 
-(defvar elpaca-menu-melpa--index-cache
+(defvar elpaca-menu-melpa-repo-url  "https://github.com/melpa/melpa.git" "URL for MELPA repo.")
+(defvar elpaca-menu-melpa-metadata-url "https://melpa.org/archive.json" "URL for ELPA metadata.")
+(defvar elpaca-menu-melpa-name "MELPA" "Display name of the ELPA.")
+(defvar elpaca-menu-melpa-branch-name "master" "Name of MELPA repo branch to check out.")
+(defvar elpaca-menu-melpa-index-cache
   (elpaca--read-file (expand-file-name "melpa.eld" elpaca-cache-directory))
   "MELPA recipe cache.")
-
+(defvar elpaca-menu-melpa-metadata-formatter nil
+  "Function which, given metadta, returns alist of form: ((PKG-SYMBOL . data) ...).")
 (defvar url-http-end-of-headers)
+
 (defun elpaca-menu-melpa--metadata ()
   "Return an alist of MELPA package metadata."
-  (with-current-buffer (url-retrieve-synchronously "https://melpa.org/archive.json" 'silent)
+  (with-current-buffer (url-retrieve-synchronously elpaca-menu-melpa-metadata-url 'silent)
     (let ((s (decode-coding-region url-http-end-of-headers (point-max) 'utf-8 t)))
       (if (fboundp #'json-parse-string)
           (json-parse-string s :object-type 'alist)
@@ -47,10 +53,11 @@
   (let ((default-directory path))
     (make-directory path t)
     (make-directory (expand-file-name ".git/info/" path) t)
-    (message "Downloading MELPA recipes...")
+    (message "Downloading %s recipes..." elpaca-menu-melpa-name)
     (let* ((process-environment ;; ignore user git config see: https://github.com/progfolio/elpaca/issues/129
             (append '("GIT_CONFIG_SYSTEM=/dev/null" "GIT_CONFIG_GLOBAL=/dev/null")
-                                        process-environment))
+                    process-environment))
+           (branch elpaca-menu-melpa-branch-name)
            (processes
             (list
              (elpaca-process-call "git" "init")
@@ -59,19 +66,19 @@
                (insert "recipes")
                (append-to-file (point-min) (point-max)
                                (expand-file-name ".git/info/sparse-checkout" path)))
-             (elpaca-process-call "git" "remote" "add" "origin" "https://github.com/melpa/melpa.git")
-             (elpaca-process-call "git" "pull" "--depth=1" "origin" "master")
-             (elpaca-process-call "git" "checkout" "master")
-             (elpaca-process-call "git" "branch" "--set-upstream-to" "origin/master" "master")))
+             (elpaca-process-call "git" "remote" "add" "origin" elpaca-menu-melpa-repo-url)
+             (elpaca-process-call "git" "pull" "--depth=1" "origin" branch)
+             (elpaca-process-call "git" "checkout" branch)
+             (elpaca-process-call "git" "branch" "--set-upstream-to" (format "origin/%s" branch) branch)))
            (err (car (cl-remove-if #'zerop (delq nil processes) :key #'car))))
-      (when err (error "Unable to clone MELPA: %S" err))
-      (message "Downloading MELPA recipes...100%%"))))
+      (when err (error "Unable to clone %s: %S" elpaca-menu-melpa-name err))
+      (message "Downloading %s recipes...100%%" elpaca-menu-melpa-name))))
 
 (defun elpaca-menu-melpa--update ()
   "Update recipes in MELPA menu."
-  (message "Downloading MELPA...")
+  (message "Downloading %s..." elpaca-menu-melpa-name)
   (elpaca-with-process-call ("git" "pull")
-    (message (if success "Downloading MELPA...100%%" "Unable to pull MELPA recipes"))))
+    (message (if success "Downloading %s...100%%" "Unable to pull %s recipes") elpaca-menu-melpa-name)))
 
 (defun elpaca-menu-melpa--convert (file metadata)
   "Return menu item candidate for FILE's MELPA recipe and METADATA."
@@ -86,7 +93,7 @@
                 (append (list :package (symbol-name package)) recipe))
           (unless (plist-member recipe :files)
             (setq recipe (plist-put recipe :files elpaca-default-files-directive)))
-          (let ((candidate (list :source "MELPA" :recipe recipe)))
+          (let ((candidate (list :source elpaca-menu-melpa-name :recipe recipe)))
             (when-let ((data (alist-get package metadata)))
               (setq candidate
                     (append candidate
@@ -107,27 +114,32 @@
 
 (defun elpaca-menu-melpa--index ()
   "Return candidate list of available MELPA recipes."
-  (or elpaca-menu-melpa--index-cache
-      (prog1
-          (setq elpaca-menu-melpa--index-cache
-                (cl-loop with metadata = (elpaca-menu-melpa--metadata)
-                         for file in (directory-files "./recipes/" 'full "\\(?:\\`[^.]\\)")
-                         for candidate = (elpaca-menu-melpa--convert file metadata)
-                         when candidate collect candidate))
-        (elpaca--write-file (expand-file-name "melpa.eld" elpaca-cache-directory)
-          (prin1 elpaca-menu-melpa--index-cache)))))
+  (or elpaca-menu-melpa-index-cache
+      (prog1 (setq elpaca-menu-melpa-index-cache
+                   (cl-loop with metadata =
+                            (if elpaca-menu-melpa-metadata-formatter
+                                (funcall elpaca-menu-melpa-metadata-formatter
+                                         (elpaca-menu-melpa--metadata))
+                              (elpaca-menu-melpa--metadata))
+                            for file in (directory-files "./recipes/" 'full "\\(?:\\`[^.]\\)")
+                            for candidate = (elpaca-menu-melpa--convert file metadata)
+                            when candidate collect candidate))
+        (elpaca--write-file (expand-file-name (format "%s.eld" (downcase elpaca-menu-melpa-name))
+                                              elpaca-cache-directory)
+          (prin1 elpaca-menu-melpa-index-cache)))))
 
 ;;;###autoload
 (defun elpaca-menu-melpa (request)
   "Delegate REQUEST.
 If REQUEST is `index`, return a recipe candidate alist.
 If REQUEST is `update`, update the MELPA recipe cache."
-  (let* ((repo (expand-file-name "melpa/" elpaca-cache-directory))
+  (let* ((name (file-name-sans-extension (file-name-nondirectory elpaca-menu-melpa-repo-url)))
+         (repo (expand-file-name name elpaca-cache-directory))
          (default-directory repo))
     (unless (file-exists-p repo) (elpaca-menu-melpa--clone repo))
     (pcase request
       ('index  (elpaca-menu-melpa--index))
-      ('update (setq elpaca-menu-melpa--index-cache nil)
+      ('update (setq elpaca-menu-melpa-index-cache nil)
                (elpaca-menu-melpa--update)))))
 
 (provide 'elpaca-menu-melpa)
