3656cb9c90689b5ba29d14f4ff916c412ec045f3
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2023-12-19 20:18:24 -0500

    elpaca-ignored-dependencies: default to package--builtin-versions
    Should be more thorough than maintaining our own list.

--- elpaca.el
+++ elpaca.el
@@ -191,12 +191,7 @@ Each function is passed a request, which may be any of the following symbols:
   :type 'integer)
 (defcustom elpaca-default-remote-name "origin" "Default remote name." :type 'string)
 
-(defcustom elpaca-ignored-dependencies
-  `( emacs cl-lib cl-generic nadvice org org-mode map seq json project auth-source-pass
-     let-alist flymake jsonrpc eldoc erc ntlm python so-long soap-client
-     svg ruby-mode verilog-mode xref
-     ,@(unless (< emacs-major-version 28) '(transient))
-     ,@(unless (< emacs-major-version 29) '(external-completion use-package bind-key eglot)))
+(defcustom elpaca-ignored-dependencies (mapcar #'car package--builtin-versions)
   "List of IDs which are not installed unless the user explicitly requests them."
   :type '(repeat symbol))
 
