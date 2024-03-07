c6f5d7d147d20935d5d82b5324f71f2889837299
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2024-03-07 18:33:29 -0500

    elpaca-process-poll: require subr-x on Emacs 27
    Otherwise string-trim is not available.
    Fixes #277

--- elpaca-process.el
+++ elpaca-process.el
@@ -82,6 +82,7 @@ This allows for output to be passed back to the parent Emacs process."
   (let* ((program (if (string-match-p "/" program) (expand-file-name program) program))
          (subprocess
           `(with-temp-buffer
+             (when (< emacs-major-version 28) (require 'subr-x)) ;;@COMPAT: Emacs 27
              (setq load-prefer-newer t)
              (let ((p (make-process
                        :name   ,(concat "elpaca-process-poll-" program)
