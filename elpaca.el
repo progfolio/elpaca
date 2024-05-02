5e1f4449c47d6e40ce86b2b18e9b93fa9dbead96
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2024-05-02 14:08:41 -0400

    formatting

--- elpaca.el
+++ elpaca.el
@@ -41,8 +41,8 @@
 (defvar elpaca-installer-version -1)
 (or noninteractive (= elpaca-installer-version 0.7)
     (lwarn '(elpaca installer) :warning "%s installer version does not match %s."
-          (or (symbol-file 'elpaca-installer-version 'defvar) "Init")
-          (expand-file-name "./doc/installer.el" (file-name-directory (or load-file-name (buffer-file-name))))))
+           (or (symbol-file 'elpaca-installer-version 'defvar) "Init")
+           (expand-file-name "./doc/installer.el" (file-name-directory (or load-file-name (buffer-file-name))))))
 (or (executable-find "git") (error "Elpaca unable to find git executable"))
 (and (not after-init-time) load-file-name (featurep 'package) (warn "Package.el loaded before Elpaca"))
 
