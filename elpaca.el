da56eaf74af910616cf60d0c8475561856bcdf9a
Martin Edström <meedstrom91@gmail.com>
2024-09-26 23:21:04 +0200

    Mostly sync with MELPA :version-regexp
    Excludes MELPA's %p, which they substitute for the package name,
    enabling tags like "elpaca-0.2" if the package is named elpaca.

--- elpaca.el
+++ elpaca.el
@@ -1174,7 +1174,9 @@ The keyword's value is expected to be one of the following:
                           (elpaca--directory-files-recursively repo (concat "\\`[^.z-a]*" name))))
                     (error "Unable to find main elisp file for %S" package)))))))
 
-(defvar elpaca--tag-regexp "\\(?:v?\\([[:digit:]]+\\.[[:digit:]]+\\.[[:digit:]]+\\)\\)")
+;; See `package-build-version-regexp' in https://github.com/melpa/melpa/blob/c84e975e5cd3ff264a134e4608041947ef010f01/package-build/package-build.el#L250
+(defvar elpaca--tag-regexp
+  "\\`\\(?:\\|[RVrv]\\|release[/-]v?\\)?\\(?1:[0-9]+\\(\\.[0-9]+\\)*\\)\\'")
 (defun elpaca-latest-tag (e)
   "Return E's latest merged tag matching recipe tag regexp or `elpaca--tag-regexp'."
   (when-let ((default-directory (elpaca<-repo-dir e))
